315bc2c18bd79073dac3fa7fb60631a8ebfbbb20
Roy-Ghandour <ghandourroy@gmail.com>
2024-08-29 09:56:07 +0400

    added stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, inject, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PLATFORM_ID } from '@angular/core';
+import { platform } from 'os';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'project1';
+
+  constructor() {
+    const platformId = inject(PLATFORM_ID);
+    if (isPlatformBrowser(platformId)) localStorage.setItem('test', '1');
+  }
+
+  ngOnInit() {
+    console.log('HELLOOOOOOOOO');
+  }
+
+  handleClick() {
+    console.log('sdjflksjkl');
+  }
 }
